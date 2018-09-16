f002c04f34d97fb9e2ecbd7c59ea2bbcb7ca0096
Mariana Facchina <marianafacchina@facchina.local>
2018-09-16 18:17:08 -0300

    arrumado um bug

--- MyOwnMalloc/MyOwnMalloc/mymalloc.h
+++ MyOwnMalloc/MyOwnMalloc/mymalloc.h
@@ -1,38 +1,51 @@
+//
+//  myMalloc.h
+//  MyOwnMalloc
+//
+//  Created by Mariana Facchina on 16/09/2018.
+//  Copyright Â© 2018 mariana-facchina. All rights reserved.
+//
+
+#ifndef myMalloc_h
+#define myMalloc_h
+
+
 /*
  * the reference tutorial: https://bit.ly/2oMVY0U
- * A estratégia de alocação utilizada é a first fit 
- * Esta estratégia aloca o espaço requerido na primeira partição 
- * que o couber, independente do espaço livre que vai deixar. 
- * Das 3 estratégias (worst-fit, best-fit e first-fit), 
- * esta é a mais rápida, pois consome menos recursos do sistema.
+ * A estratÃ©gia de alocaÃ§Ã£o utilizada Ã© a first fit
+ * Esta estratÃ©gia aloca o espaÃ§o requerido na primeira partiÃ§Ã£o
+ * que o couber, independente do espaÃ§o livre que vai deixar.
+ * Das 3 estratÃ©gias (worst-fit, best-fit e first-fit),
+ * esta Ã© a mais rÃ¡pida, pois consome menos recursos do sistema.
  */
 
 #include<stdio.h>
 #include<stddef.h>
 
-#define MEMORYLENGHT 800
+#define MEMORYLENGHT 20000
 //alocando 20.000 bytes de memoria, uma vez que um char equivale a 1 byte
 char memory[MEMORYLENGHT];
 
 struct block {
-	//tamanho do bloco alocado
-	size_t size; 
-	//flag para saber quando o bloco esta livre ou nao (1: livre, 0: ocupado).
-	int free;   
-	//ponteiro para o prox block
-	struct block *next; 
+    //tamanho do bloco alocado
+    size_t size;
+    //flag para saber quando o bloco esta livre ou nao (1: livre, 0: ocupado).
+    int free;
+    //ponteiro para o prox block
+    struct block *next;
 };
 
-/* Inicializando um ponteiro do tipo block, que aponta para o endereco inicial do espaço alocado por memory.
-   A ideia eh que freeList aponte para o inicio da lista encadeada de blocks, e por isso, o endereco inicial de memory
-   deve ser convertido para o tipo void para que aloquemos blocks de tipos de dados diferentes (char, double, int, etc).
-*/
+/* Inicializando um ponteiro do tipo block, que aponta para o endereco inicial do espaÃ§o alocado por memory.
+ A ideia eh que freeList aponte para o inicio da lista encadeada de blocks, e por isso, o endereco inicial de memory
+ deve ser convertido para o tipo void para que aloquemos blocks de tipos de dados diferentes (char, double, int, etc).
+ */
 struct block *freeList = (void*)memory;
 
 //Definicao de funcoes
 void initialize();
-void splitMemoryBlock(struct block *slotMaisApropriado, size_t tamanhoRequerido);
-void *Aloca(size_t sizeRequired);
+void splitMemoryBlock(struct block *fitting_slot,size_t size);
+void *aloca(size_t sizeRequired);
 void mergeMemoryBlocks();
-void Libera(void* ptr);
+void libera(void* ptr);
 
+#endif /* myMalloc_h */
